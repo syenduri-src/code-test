d2e003084ba98860b4decff08387af8a859d6c90
syenduri-src <srikanthyenduri@gmail.com>
2021-05-04 18:17:07 -0400

    String Operations

---
+++ src/com/code/test/string/StringOperations.java
@@ -0,0 +1,35 @@
+package com.code.test.string;
+
+public class StringOperations {
+
+    public String swapLastTwoCharacters(String str) {
+
+        int length = str.length();
+
+        if(length < 2) return str;
+
+        char lastChar = str.charAt(length - 1);
+
+        char secondLastChar = str.charAt(length - 2);
+
+        String restOfTheString = str.substring(0, length - 2);
+
+        return restOfTheString + lastChar + secondLastChar;
+
+    }
+
+    public String truncateAInFirst2Positions(String str) {
+
+        if(str.length()<2)
+            return str.replaceAll("A", "");
+
+        String first2Characters = str.substring(0, 2);
+
+        String first2CharactersUpdated = first2Characters.replaceAll("A", "");
+
+        String restOfTheString = str.substring(2);
+
+        return first2CharactersUpdated + restOfTheString;
+    }
+
+}
